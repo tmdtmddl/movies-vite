d62d0eacb97d907c84b68521fee0e17c2dfa75a7
tmdtmddl <tmddlqkqh@naver.com>
2025-04-02 20:50:52 +0900

    add

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: "/movies-vite/",
+  base: "/movies-vite/",
 });
